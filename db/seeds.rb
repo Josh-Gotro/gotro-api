dc87f10fbb40a2dbab15bb0b4035c076570cae2f
Josh-Gotro <joshuagauthreaux@gmail.com>
2020-07-04 09:49:28 -0500

    nav bar fixed, transparent, smooth scrolling, component transitions

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,8 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Message.delete_all
+
+10.times do 
+    Message.create(name: Faker::Books::Dune.unique.character, email: Faker::Internet.unique.email , message: Faker::Books::Dune.unique.quote)
+end
