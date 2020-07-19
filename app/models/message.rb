2a30398f1e81db1538ba344f13c69623b905bceb
Josh-Gotro <joshuagauthreaux@gmail.com>
2020-07-18 23:28:37 -0500

    closer to validations

--- app/models/message.rb
+++ app/models/message.rb
@@ -2,8 +2,9 @@ class Message < ApplicationRecord
     validates :name, presence: true
     validates :comment, presence: true
     validates :email, presence: true
-    validates :email, format: { with: /^(.+)@(.+)$/ }
-    # validates :email, length: { minimum: 4, maximum: 254 }  
+    validates :email, format: { with: /^[\\w!#$%&'*+/=?`{|}~^-]+(?:\\.[\\w!#$%&'*+/=?`{|}~^-]+)*@(?:[a-zA-Z0-9-]+\\.)+[a-zA-Z]{2,6}$/,
+    message: "enter a valid email"  }
+
 
     # Declare the e-mail headers. It accepts anything the mail method
     # in ActionMailer accepts.
