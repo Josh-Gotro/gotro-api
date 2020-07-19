b84505393a318181d3cb39c7cc87f9a26792fe17
Josh-Gotro <joshuagauthreaux@gmail.com>
2020-07-18 23:05:46 -0500

    heroku validations tweaking

--- app/models/message.rb
+++ app/models/message.rb
@@ -1,7 +1,9 @@
 class Message < ApplicationRecord
     validates :name, presence: true
     validates :comment, presence: true
-    validates :email, presence: true
+    validates :email, format: { with: /^(.+)@(.+)$/ },
+    validates :email, uniqueness: { case_sensitive: false },
+    validates :email, length: { minimum: 4, maximum: 254 }  
 
     # Declare the e-mail headers. It accepts anything the mail method
     # in ActionMailer accepts.
