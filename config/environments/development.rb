d4e142d0f89679c18c3760ed528be831943fa86c
Josh-Gotro <joshuagauthreaux@gmail.com>
2020-07-22 21:51:55 -0500

    force ssl

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -49,7 +49,7 @@ Rails.application.configure do
   # Use an evented file watcher to asynchronously detect changes in source code,
   # routes, locales, etc. This feature depends on the listen gem.
   config.file_watcher = ActiveSupport::EventedFileUpdateChecker
-
+config.force_ssl = true
     config.action_mailer.perform_deliveries = true
   config.action_mailer.raise_delivery_errors= true
   config.action_mailer.delivery_method = :smtp
